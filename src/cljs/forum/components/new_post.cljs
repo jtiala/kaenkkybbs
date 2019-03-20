9825437f97760182e17d9aed519676ee343d7752
Joonas Tiala <joonas.tiala@solita.fi>
2019-03-20 18:43:23 +0200

    Use logged in user when posting

--- src/cljs/forum/components/new_post.cljs
+++ src/cljs/forum/components/new_post.cljs
@@ -1,21 +1,7 @@
 (ns forum.components.new-post
   (:require [forum.actions :as actions]
-            [forum.utils :as utils]
             [clojure.string :refer [trim]]
             [reagent.core :as reagent :refer [atom]]))
-
-(defn create-user-option [[id name] component-state]
-  [:option {:value id} name])
-
-(defn posted-by-select [component-state]
-  [:select {:id "posted_by"
-            :name "posted_by"
-            :value (:posted-by @component-state)
-            :on-change #(swap! component-state assoc :posted-by (-> % .-target .-value utils/parse-int))}
-   (create-user-option [0 "<Anonymous>"] component-state)
-   (create-user-option [1 "Kaenkkykeisari"] component-state)
-   (create-user-option [2 "ananas666"] component-state)
-   (create-user-option [3 "PizzaKuski"] component-state)])
 
 (defn message-textarea [component-state]
   [:textarea {:id "message"
@@ -35,13 +21,11 @@
 (defn component [state]
   (let [component-state (atom {:message ""
                                :thread (get-in @state [:thread :id])
-                               :posted-by 0})]
+                               :posted-by (get-in @state [:user :id] 0)})]
     (fn []
       [:div {:class "new-post"}
        [:h3 "Write a response"]
        [:form
-        [:label {:for "posted_by"} "Send as (tmp)"]
-        [posted-by-select component-state]
         [:label {:for "message"} "Your message"]
         [message-textarea component-state]
         [submit-button component-state]]])))
--- src/cljs/forum/components/new_thread.cljs
+++ src/cljs/forum/components/new_thread.cljs
@@ -1,21 +1,7 @@
 (ns forum.components.new_thread
   (:require [forum.actions :as actions]
-            [forum.utils :as utils]
             [clojure.string :refer [trim]]
             [reagent.core :as reagent :refer [atom]]))
-
-(defn create-user-option [[id name] component-state]
-  [:option {:value id} name])
-
-(defn started-by-select [component-state]
-  [:select {:id "started_by"
-            :name "started_by"
-            :value (:started-by @component-state)
-            :on-change #(swap! component-state assoc :started-by (-> % .-target .-value utils/parse-int))}
-   (create-user-option [0 "<Anonymous>"] component-state)
-   (create-user-option [1 "Kaenkkykeisari"] component-state)
-   (create-user-option [2 "ananas666"] component-state)
-   (create-user-option [3 "PizzaKuski"] component-state)])
 
 (defn title-input [component-state]
   [:input {:id "title"
@@ -37,19 +23,17 @@
             :on-click (fn [event]
                         (.preventDefault event)
                         (actions/create-thread (trim (:title @component-state)) (:started-by @component-state) (trim (:message @component-state)))
-                        (swap! component-state assoc :title "" :message "" :started-by 0))}
+                        (swap! component-state assoc :title "" :message ""))}
    "Send"])
 
 (defn component [state]
   (let [component-state (atom {:title ""
-                               :started-by 0
+                               :started-by (get-in state [:user :id] 0)
                                :message ""})]
     (fn []
       [:div {:class "new-thread"}
        [:h3 "Start a new thread"]
        [:form
-        [:label {:for "started_by"} "Send as (tmp)"]
-        [started-by-select component-state]
         [:label {:for "title"} "Title"]
         [title-input component-state]
         [:label {:for "message"} "Your message"]
