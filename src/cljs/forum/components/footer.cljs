fa86ceb4dd2587d9cfa5ba282ea3c76c5712ac09
Joonas Tiala <joonas.tiala@solita.fi>
2019-03-29 10:08:56 +0200

    Add github link to the footer

--- src/cljs/forum/components/footer.cljs
+++ src/cljs/forum/components/footer.cljs
@@ -3,4 +3,6 @@
 (defn component []
   [:footer#footer
    [:span.logo "🍕"]
-   [:span.copyright "© KaenkkyBBS 2019."]])
+   [:span.copyright "© KaenkkyBBS 2019."]
+   [:span.links
+    [:a {:href "https://github.com/jtiala/kaenkkybbs" :target "_blank"} "GitHub"]]])
